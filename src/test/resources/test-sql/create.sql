82cc990dd79a4eef408252ea13f86f3668297b7c
Alexandra-Pop <100316034+Alexandra-Pop@users.noreply.github.com>
2022-11-15 04:11:59 +0200

    Delete src directory

--- src/test/resources/test-sql/create.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO User (id, name, address, age, email, password, role) values
-    ('00201D1E61F3401588BF4292B86E22E4', 'My Name', 'My Address', 20, 'email@yahoo.com', 'Aabv-ads1', 'client');
