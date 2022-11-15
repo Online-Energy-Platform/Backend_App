932f2d9d30bf9bfcf9d1129fe69161133ab50e7b
Alexandra-Pop <100316034+Alexandra-Pop@users.noreply.github.com>
2022-11-15 05:24:46 +0200

    Add files via upload

--- src/test/resources/test-sql/create.sql
+++ src/test/resources/test-sql/create.sql
@@ -1,2 +1,2 @@
-INSERT INTO User (id, email, first_name, last_name, password) values
-    ('0827d1da-4137-4da8-a546-bf4438eb477f', 'email@yahoo.com', 'Firstname', 'Lastname', 'parola');
+INSERT INTO User (id, email, first_name, last_name, password, role_id) values
+    ('0827d1da-4137-4da8-a546-bf4438eb477f', 'email@yahoo.com', 'Firstname', 'Lastname', 'parola', '122e4563-e95c-12d3-a456-556642440011');
