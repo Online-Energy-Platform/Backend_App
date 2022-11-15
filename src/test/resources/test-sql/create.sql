0d6c907d95e36cfba435ce9aac6e6eec1fe25757
Alexandra-Pop <100316034+Alexandra-Pop@users.noreply.github.com>
2022-11-15 03:03:33 +0200

    Update create.sql

--- src/test/resources/test-sql/create.sql
+++ src/test/resources/test-sql/create.sql
@@ -1,2 +1,2 @@
-INSERT INTO User (id, name, address, age, email, password, role) values
-    ('00201D1E61F3401588BF4292B86E22E4', 'My Name', 'My Address', 20, 'email@yahoo.com', 'Aabv-ads1', 'client');
+INSERT INTO User (id, firstName, lastName, email, password) values
+    ('00201D1E61F3401588BF4292B86E22E4', 'My Name', 'My Address', 'email@yahoo.com', 'Aabv-ads1');
